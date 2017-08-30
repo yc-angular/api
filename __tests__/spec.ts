55bf388cee21e5d38414c7331ae88c70922152e5
Yu Chen <yu.chen@live.ie>
2017-08-30 22:43:09 +0800

    fixed aot

--- __tests__/spec.ts
+++ __tests__/spec.ts
@@ -1,8 +1,29 @@
 import { inject, TestBed } from '@angular/core/testing';
 
-import { ApiModule, Api } from '../src/';
+import { ApiModule, Api, ROOT } from '../src/';
 
-describe('SampleComponent', () => {
+describe('default', () => {
+  let api: Api;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        ApiModule.forRoot()
+      ]
+    });
+  });
+  
+  beforeEach(inject([Api], _api => {
+    api = _api;
+  }));
+
+  it('Should be initialised', () => {
+    expect(api).toBeDefined();
+    expect(api.root).toBe(ROOT);
+  });
+});
+
+describe('custom', () => {
   let api: Api;
   const host = 'http://localhost';
 
